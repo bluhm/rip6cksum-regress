61dc80430e08df7dd2af3ccb9862b14f23bb990b
Alexander Bluhm <alexander.bluhm@gmx.net>
2019-04-20 17:02:59 +0200

    Do getopt and open socket.

--- rip6-cksum.c
+++ rip6-cksum.c
@@ -15,10 +15,79 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
+#include <err.h>
+#include <limits.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
+#include <netinet/in.h>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+
+void __dead usage(void);
+
+void __dead
+usage(void)
+{
+	fprintf(stderr, "rip6-cksum [-es] [-c ckoff] [-w wait]\n"
+	    "    -c ckoff   set checksum offset within rip header\n"
+	    "    -e         expect error when setting ckoff\n"
+	    "    -s         send packet on socket\n"
+	    "    -w wait    wait for packet on socket, timeout in seconds\n"
+	);
+	exit(1);
+}
+
+const struct in6_addr loop6 = IN6ADDR_LOOPBACK_INIT;
 int
 main(int argc, char *argv[])
 {
+	int s, ch, eflag, ckoff, sflag, wait;
+	const char *errstr;
+	struct sockaddr_in6 sin6;
+
+	eflag = ckoff = sflag = wait = 0;
+	while ((ch = getopt(argc, argv, "c:esw:")) != -1) {
+		switch (ch) {
+		case 'c':
+			ckoff = strtonum(optarg, INT_MIN, INT_MAX, &errstr);
+			if (errstr != NULL)
+				errx(1, "ckoff is %s: %s", errstr, optarg);
+			break;
+		case 'e':
+			eflag = 1;
+			break;
+		case 's':
+			sflag = 1;
+			break;
+		case 'w':
+			wait = strtonum(optarg, INT_MIN, INT_MAX, &errstr);
+			if (errstr != NULL)
+				errx(1, "wait is %s: %s", errstr, optarg);
+			break;
+		default:
+			usage();
+		}
+	}
+	argc -= optind;
+	argv += optind;
+	if (argc) {
+		fprintf(stderr, "too many arguments\n");
+		usage();
+	}
+
+	s = socket(AF_INET6, SOCK_RAW, 255);
+	if (s == -1)
+		err(1, "socket raw");
+	memset(&sin6, 0, sizeof(sin6));
+	sin6.sin6_family = AF_INET6;
+	sin6.sin6_addr = loop6;
+	if (bind(s, (struct sockaddr *)&sin6, sizeof(sin6)) == -1)
+		err(1, "bind ::1");
+
+
 	return 0;
 }
